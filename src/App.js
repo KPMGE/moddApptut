126a39d1da2dcba1e610a9b588d815e8863be957
KPMGE <jesuskevin254@gmail.com>
2024-12-22 12:11:32 -0300

    :sparkles: add KTC token implementation

--- src/App.js
+++ src/App.js
@@ -1,79 +1,86 @@
 import './App.css';
-import {ethers} from 'ethers'
+import { ethers } from 'ethers'
 import { useState } from 'react';
-import TokenArtifact from "./artifacts/contracts/Token.sol/Token.json"
-const tokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+import TokenArtifact from "./artifacts/contracts/KevinToken.sol/KevinToken.json"
+const tokenAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3"
 
 const localBlockchainAddress = 'http://localhost:8545'
 
 function App() {
+    const [tokenData, setTokenData] = useState({})
+    const [amount, setAmount] = useState()
+    const [userAccountId, setUserAccountId] = useState()
 
- const [tokenData, setTokenData] = useState({})
- const [amount, setAmount] = useState()
- const [userAccountId, setUserAccountId] = useState()
+    const provider = new ethers.providers.JsonRpcProvider(localBlockchainAddress)
+    const signer = provider.getSigner();
 
- const provider = new ethers.providers.JsonRpcProvider(localBlockchainAddress)
+    async function _intializeContract(init) {
+        // We first initialize ethers by creating a provider using window.ethereum
+        // When, we initialize the contract using that provider and the token's
+        // artifact. You can do this same thing with your contracts.
+        const contract = new ethers.Contract(
+            tokenAddress,
+            TokenArtifact.abi,
+            init
+        );
 
- const signer = provider.getSigner();
+        return contract
+    }
 
- async function _intializeContract(init) {
-  // We first initialize ethers by creating a provider using window.ethereum
+    async function _getTokenData() {
+        const contract = await _intializeContract(signer)
 
-  // When, we initialize the contract using that provider and the token's
-  // artifact. You can do this same thing with your contracts.
-  const contract = new ethers.Contract(
-    tokenAddress, 
-    TokenArtifact.abi, 
-    init
-  );
-  
-  return contract
-}
+        const name = await contract.name();
+        const symbol = await contract.symbol();
+        const tokenData = { name, symbol }
 
- async function _getTokenData() {
-  const contract = await _intializeContract(signer)
-  const name = await contract.name();
-  const symbol = await contract.symbol();
-  const tokenData = {name, symbol}
+        setTokenData(tokenData);
+    }
 
-  setTokenData(tokenData);
-}
+    async function requestAccount() {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+    }
 
-async function requestAccount() {
-  await window.ethereum.request({ method: 'eth_requestAccounts' });
-}
+    async function sendMDToken() {
+        if (typeof window.ethereum !== 'undefined') {
+            await requestAccount()
+            const contract = await _intializeContract(signer)
+            const transaction = await contract.transfer(userAccountId, amount);
+            await transaction.wait();
+            console.log(`${amount} MDToken has been sent to ${userAccountId}`);
+        }
+    }
 
-async function sendMDToken() {
-  if (typeof window.ethereum !== 'undefined') {
-    await requestAccount()
-    const contract = await _intializeContract(signer)
-    const transaction = await contract.transfer(userAccountId, amount);
-    await transaction.wait();
-    console.log(`${amount} MDToken has been sent to ${userAccountId}`);
-  }
-}
+    async function getBalance() {
+        if (typeof window.ethereum !== 'undefined') {
+            const contract = await _intializeContract(signer)
+            const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' })
+            const balance = await contract.balanceOf(account);
+            console.log("Account Balance: ", balance.toString());
+        }
+    }
 
-async function getBalance() {
-  if (typeof window.ethereum !== 'undefined') {
-    const contract = await _intializeContract(signer)
-    const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    const balance = await contract.balanceOf(account);
-    console.log("Account Balance: ", balance.toString());
-  }
-}
-  return (
-    <div className="App">
-      <header className="App-header">
-      <button onClick={_getTokenData}>get token data</button>
-      <h1>{tokenData.name}</h1>
-      <h1>{tokenData.symbol}</h1>  
-      <button onClick={getBalance}>Get Balance</button>  
-      <button onClick={sendMDToken}>Send MDToken</button>
-      <input onChange={e => setUserAccountId(e.target.value)} placeholder="Account ID" />
-        <input onChange={e => setAmount(e.target.value)} placeholder="Amount" />
-      </header>
-    </div>
-  );
+    async function issueToken(amount) {
+        const contract = await _intializeContract(signer)
+        const kthAmount = 10000000000000
+        await contract.functions.issueToken(kthAmount)
+        console.log('Issue token successfull')
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button onClick={issueToken}>issueToken</button>
+                <button onClick={_getTokenData}>get token data</button>
+                <h1>{tokenData.name}</h1>
+                <h1>{tokenData.symbol}</h1>
+                <button onClick={getBalance}>Get Balance</button>
+                <button onClick={sendMDToken}>Send MDToken</button>
+                <input onChange={e => setUserAccountId(e.target.value)} placeholder="Account ID" />
+                <input onChange={e => setAmount(e.target.value)} placeholder="Amount" />
+            </header>
+        </div>
+    );
 }
 
 export default App;
